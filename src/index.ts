e49b92bff6118eb6c13a688f3587ed5b5fe5e4ea
ytnobody <ytnobody@gmail.com>
2023-11-02 09:28:44 +0000

    basic features implemented

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,46 @@
-import WebSocket, { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws'
+import { ulid } from 'ulid'
 
-const wss = new WebSocketServer({ port: 8080 });
+const port = 8080
 
+const wss = new WebSocketServer({ port })
+
+const max_score = 20
+const max_vote_by_user = 2
+const vote_list: string[] = []
+const connections: string[] = []
+
+wss.on('listening', () => console.log(`listening on port ${port}`))
 wss.on('connection', function connection(ws: WebSocket) {
-  ws.on('error', console.error);
+  ws['id'] = ulid()
+  console.log(`connected: ${ws.id}`)
+  connections.push(ws.id)
+  ws.send(`code:${ws.id}`)
 
-  ws.send(`connection established`)
+  ws.on('error', console.error)
+  ws.on('close', function close() {
+    console.log(`disconnected: ${ws.id}`)
+    connections.splice(connections.indexOf(ws.id), 1)
+  })
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    ws.send(`server received: ${data}`);
-  });
+    if (ws.id && connections.includes(ws.id)) {
+      if (data == ws.id) {
+        if (vote_list.filter(id => id == ws.id).length < max_vote_by_user && vote_list.length < max_score) {
+          vote_list.push(ws.id)
+          console.log(`vote: ${ws.id}`)
+          console.log(`score: ${vote_list.length}`)
+          ws.send(`score: ${vote_list.length}`)
+        }
+      }
+      if (data == 'reset') {
+        vote_list.length = 0
+        console.log(`reset`)
+      }
+    } else {
+      console.log(`illegal connection`)
+      ws.disconnect()
+    }
+  })
 
-});
+})
