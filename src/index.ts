76e90bd58daf9369f22ae07f45ee96d4d9357017
ytnobody <ytnobody@gmail.com>
2023-11-02 08:55:39 +0000

    move to src

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws: WebSocket) {
   ws.on('error', console.error);
+
+  ws.send(`connection established`)
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
